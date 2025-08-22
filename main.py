150d69774174b74f28371c862fc57859f66a7e33
Afigueroaf <andresfig03@gmail.com>
2025-08-21 21:25:58 -0500

    Actualización 1

--- main.py
+++ main.py
@@ -206,6 +206,6 @@ if __name__ == "__main__":
     print("📍 API: http://localhost:8000")
     print("📚 Docs: http://localhost:8000/docs")
     print("🗺️ Mapa: http://localhost:8000/mapa")
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
 
     print("primer pull request")
